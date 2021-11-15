a745b59325f9a3b30ee328ef659c69cc193bf9fd
ogonzalez03 <orlandogomo@gmail.com>
2021-11-14 22:51:02 -0500

    BD models, routes, controllers, add express-promise-router module

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,22 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express-promise-router')();
+
+const {
+    index,
+    newUser,
+    getUser,
+    replaceUser,
+    deleteUser,
+    getUserCars,
+    newUserCar
+} = require('../controllers/user');
+
+router.get('/', index);
+router.post('/', newUser);
+router.get('/:userId', getUser);
+router.put('/:userId', replaceUser);
+router.delete('/:userId', deleteUser);
+
+router.get('/:userId/cars', getUserCars);
+router.post('/:userId/cars', newUserCar);
 
 module.exports = router;
